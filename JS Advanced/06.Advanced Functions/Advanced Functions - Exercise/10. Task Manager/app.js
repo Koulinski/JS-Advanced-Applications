b582130c959d7b280a197caa12bf81c45501e01c
Anzhela Nurieva <anzzhhela98@gmail.com>
2021-05-21 22:35:34 +0300

    Exercise

--- JS Advanced/06.Advanced Functions/Advanced Functions - Exercise/10. Task Manager/app.js
+++ JS Advanced/06.Advanced Functions/Advanced Functions - Exercise/10. Task Manager/app.js
@@ -5,14 +5,59 @@ function solve() {
     const taskDescrip = document.getElementById('description');
 
     const addBtn = section1.children[1].querySelector('button');
-
     addBtn.addEventListener('click', onAdd);
 
     function onAdd(ev) {
         ev.preventDefault();
 
-        if (taskName.value != '' && taskDate.value && taskDescrip.value != '') {
-            [taskName, taskDate, taskDescrip].forEach((el) => el.value = '');
+        if (taskName.value == '' || taskDate.value == '' || taskDescrip.value == '') {
+            return;
         }
+
+        const article = createElement('article', null, null);
+        const h3 = createElement('h3', taskName.value, null);
+        const pDescription = createElement('p', `Description: ${taskDescrip.value}`);
+        const pDate = createElement('p', `Due Date: ${taskDate.value}`, null);
+        const buttonsDiv = createElement('buttonsDiv', null, 'flex');
+        const buttonStart = createElement('button', 'Start', 'green');
+        const buttonDelete = createElement('button', 'Delete', 'red');
+        const buttonFinish = createElement('button', 'Finish', 'orange');
+
+        buttonStart.addEventListener('click', () => {
+            section3.children[1].appendChild(article);
+            buttonStart.remove();
+            buttonsDiv.appendChild(buttonFinish);
+        });
+
+        buttonFinish.addEventListener('click', () => {
+            section4.lastElementChild.appendChild(article);
+            buttonsDiv.remove();
+        });
+
+        buttonDelete.addEventListener('click', () => {
+            article.remove();
+        });
+
+        [h3, pDescription, pDate, buttonsDiv].forEach((el) => {
+            article.appendChild(el);
+        });
+
+        [buttonStart, buttonDelete].forEach((el) => {
+            buttonsDiv.appendChild(el);
+        })
+
+        section2.children[1].appendChild(article);
+
+        [taskName, taskDate, taskDescrip].forEach((el) => el.value = '');
+    }
+
+    function createElement(type, content, className) {
+        const el = document.createElement(type);
+        if (content) {
+            el.textContent = content;
+        } if (className) {
+            el.className = className;
+        }
+        return el;
     }
 }
